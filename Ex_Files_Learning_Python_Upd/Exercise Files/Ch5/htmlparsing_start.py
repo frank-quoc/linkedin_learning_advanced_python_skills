349dce2349cc26fca2dd6c6ffaa26485fa196a73
frank-quoc <frankquocho214@gmail.com>
2020-10-22 18:00:02 -0700

    Ch5 Finished

--- Ex_Files_Learning_Python_Upd/Exercise Files/Ch5/htmlparsing_start.py
+++ Ex_Files_Learning_Python_Upd/Exercise Files/Ch5/htmlparsing_start.py
@@ -1,12 +1,52 @@
 # 
 # Example file for parsing and processing HTML
 #
+from html.parser import HTMLParser
 
+metacount = 0
+
+class MyHTMLParser(HTMLParser):
+  def handle_comment(self, data):
+    print("Encountered comment: ", data)
+    pos = self.getpos() # get position where comment was encountered
+    print("\tAt line: ", pos[0], " position ", pos[1])
+  
+  def handle_starttag(self, tag, attrs): # calls func when > reached
+    global metacount
+    
+    if tag == 'meta':
+      metacount += 1
+    
+    print("Encountered comment: ", tag)
+    pos = self.getpos() # get position where comment was encountered
+    print("\tAt line: ", pos[0], " position ", pos[1])
+    
+    if attrs.__len__() > 0:
+      print("\tAttributes:")
+      for a in attrs:
+        print("\t", a[0], "=", a[1])
+
+  def handle_endtag(self, tag):
+    print("Encountered tag: ", tag) # checks for end tags
+    pos = self.getpos() # get position where comment was encountered
+    print("\tAt line: ", pos[0], " position ", pos[1])
+
+  def handle_data(self, data):
+    if data.isspace(): # checks for white spaces
+      return
+    print("Encountered data: ", data)
+    pos = self.getpos() # get position where comment was encountered
+    print("\tAt line: ", pos[0], " position ", pos[1])
 
 def main():
   # instantiate the parser and feed it some HTML
   parser = MyHTMLParser()
-    
+  f = open("samplehtml.html") 
+  if f.mode == 'r':
+    contents = f.read()
+    parser.feed(contents) # runs through html code line by line; finds code, then calls function you override in subclass
+
+  print("Meta tags found: " + str(metacount))
 
 if __name__ == "__main__":
   main();
--- Ex_Files_Learning_Python_Upd/Exercise Files/Ch5/inetdata_start.py
+++ Ex_Files_Learning_Python_Upd/Exercise Files/Ch5/inetdata_start.py
@@ -1,6 +1,13 @@
 # 
 # Example file for retrieving data from the internet
 #
+import urllib.request # provides classes and code for http requests
+
+def main():
+  webUrl = urllib.request.urlopen("http://www.google.com")
+  print("result code: " + str(webUrl.getcode()))
+  data = webUrl.read() #html code for url
+  print(data)
 
 if __name__ == "__main__":
   main()
